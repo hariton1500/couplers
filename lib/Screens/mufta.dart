08d7d8d4f407e6c6a303645286a7ee52d2312306
hariton1500 <hariton1500@gmail.com>
2021-12-12 11:55:11 +0300

    7

--- lib/Screens/mufta.dart
+++ lib/Screens/mufta.dart
@@ -11,6 +11,7 @@ class MuftaScreen extends StatefulWidget {
 
 class _MuftaScreenState extends State<MuftaScreen> {
   int? isCableSelected;
+  
 
   @override
   Widget build(BuildContext context) {
@@ -52,6 +53,45 @@ class _MuftaScreenState extends State<MuftaScreen> {
                 MediaQuery.of(context).size.width, isCableSelected ?? -1),
           ),
         ),
+        TextButton.icon(onPressed: () {
+          showDialog<CableEnd>(context: context, builder: (builContext) {
+            String? cableName;
+            int fibersNumber = 1;
+            List<DropdownMenuItem<int>> fibersList = List.generate(24, (i) => DropdownMenuItem<int>(value: i + 1, child: Text((i + 1).toString()),));
+            return AlertDialog(
+              title: const Text('Adding of cable'),
+              content: Column(
+                children: [
+                  const Text('Direction:'),
+                  TextField(
+                    keyboardType: TextInputType.text,
+                    onChanged: (text) {cableName = text;},
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Number of Fibers:'),
+                  ),
+                  DropdownButton<int>(
+                    value: fibersNumber,
+                    onChanged: (i) {
+                      print(i);
+                      setState(() {fibersNumber = i!;print(fibersNumber);});},
+                    items: fibersList
+                  )
+                  //TextField(keyboardType: TextInputType.number, onChanged: (text) {fibersNumber = int.parse(text);}),
+                ],
+              ),
+              actions: [
+                OutlinedButton(onPressed: () {
+                  Navigator.of(context).pop();
+                }, child: const Text('Cancel')),
+                OutlinedButton(onPressed: () {Navigator.of(context).pop(CableEnd(direction: cableName!, fibersNumber: fibersNumber, sideIndex: 0));}, child: const Text('Add'))
+              ],
+            );
+          }).then((value) => setState((){
+            if (value != null) widget.mufta.cables!.add(value);
+          }));
+        }, icon: const Icon(Icons.add_outlined), label: const Text('Add cable')),
         widget.mufta.connections!.isNotEmpty
             ? TextButton.icon(
                 icon: const Icon(Icons.delete_forever_outlined),
@@ -92,9 +132,41 @@ class _MuftaScreenState extends State<MuftaScreen> {
                 icon: const Icon(Icons.change_circle_outlined),
                 label: const Text('Change side'))
             : Container(),
+        isCableSelected != null && isCableSelected! >= 0
+            ? Column(
+                children: List.generate(widget.mufta.connections!.where((connection) => (connection.connectionData[0] == isCableSelected || connection.connectionData[2] == isCableSelected)).length, (index) => Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('${widget.mufta.cables![widget.mufta.connections![index].connectionData[0]].direction}[${widget.mufta.connections![index].connectionData[1] + 1}] <---> ${widget.mufta.cables![widget.mufta.connections![index].connectionData[2]].direction}[${widget.mufta.connections![index].connectionData[3] + 1}]'),
+                    TextButton.icon(onPressed: () {
+                      setState(() {
+                        widget.mufta.connections!.removeAt(index);
+                      });
+                    }, icon: const Icon(Icons.delete_outline), label: const Text('Delete'))
+                  ],
+                ))
+              )
+            : Container(),
       ],
     );
   }
+}
+
+Widget createConnection(Mufta _mufta) {
+  return Column(
+    children: [
+      Row(
+        children: List.generate(_mufta.cables!.length, (index) => Text(_mufta.cables![index].direction)),
+      ),
+      Row(
+        children: List.generate(_mufta.cables!.length, (index) => DropdownButton<String>(
+          items: List.generate(_mufta.cables![index].fibersNumber, (index) {
+            return DropdownMenuItem<String>(child: Text(index.toString()));
+          })
+          )),
+      )
+    ]
+  );
 }
 
 class MuftaPainter extends CustomPainter {
